313d1475771343c19eee093940be0a51d75a970c
Arthur Gonze Machado <arthurgonze@gmail.com>
2026-01-25 17:16:52 -0500

    initial commit

---
+++ .nvim.lua
@@ -0,0 +1,27 @@
+local project_root = vim.fn.getcwd()
+
+vim.keymap.set("n", "<leader>ls", function()
+	vim.cmd("split | terminal live-server --port=8080 --browser=chrome")
+	vim.notify("Live server started at http://localhost:8080", vim.log.levels.INFO)
+end, { desc = "[L]ive [S]erver start" })
+
+vim.keymap.set("n", "<leader>lk", function()
+	for _, buf in ipairs(vim.api.nvim_list_bufs()) do
+		if vim.bo[buf].buftype == "terminal" then
+			local chan = vim.bo[buf].channel
+			vim.api.nvim_chan_send(chan, "\x03")
+			vim.cmd("bdelete! " .. buf)
+		end
+	end
+	vim.notify("Live server stopped", vim.log.levels.INFO)
+end, { desc = "[L]ive server [K]ill" })
+
+vim.keymap.set("n", "<leader>lo", function()
+	vim.cmd("!start http://localhost:8080")
+end, { desc = "[L]ive server [O]pen browser" })
+
+vim.keymap.set("n", "<leader>tw", function()
+	vim.cmd("split | terminal tsc --watch")
+end, { desc = "[T]ypeScript [W]atch" })
+
+print("✓ Portfolio Project loaded")
---
+++ README.md
@@ -0,0 +1,20 @@
+# Portfolio Site
+
+Personal portfolio website built with HTML, CSS, and JavaScript.
+
+## License
+MIT
+
+## Development
+
+Install dependencies:
+```bash
+npm install
+```
+
+Run Locally:
+```bash
+npm run dev
+```
+
+Or run the live-server
